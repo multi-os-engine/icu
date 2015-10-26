7564ef6bc7524021b8955d8cdd0a23e5036ae73f
Neil Fuller <nfuller@google.com>
2015-10-26 16:21:44 +0000

    Tool changes to handle making some of ICU public
    Handles the whitelisting of some classes and hiding deprecated elements
    within that set.
    
    Bug: 22023363
    Change-Id: Iafcb25530de016d04534e4d4d80c0b95a16f7f9c

--- tools/srcgen/generate_deprecation_list.sh
+++ tools/srcgen/generate_deprecation_list.sh
@@ -27,15 +27,6 @@ mm -j32
 
 ANDROID_ICU4J_DIR=${ANDROID_BUILD_TOP}/external/icu/android_icu4j
 
-# Clean out previous generated code / resources.
-DEST_SRC_DIR=${ANDROID_ICU4J_DIR}/src/main/java
-rm -rf ${DEST_SRC_DIR}
-mkdir -p ${DEST_SRC_DIR}
-
-DEST_RESOURCE_DIR=${ANDROID_ICU4J_DIR}/resources
-rm -rf ${DEST_RESOURCE_DIR}
-mkdir -p ${DEST_RESOURCE_DIR}
-
 # Generate the source code needed by Android.
 CLASSPATH=${ANDROID_BUILD_TOP}/out/host/common/obj/JAVA_LIBRARIES/currysrc_intermediates/javalib.jar:${ANDROID_BUILD_TOP}/out/host/common/obj/JAVA_LIBRARIES/android_icu4j_srcgen_intermediates/javalib.jar
 INPUT_DIRS="\
@@ -46,21 +37,4 @@ INPUT_DIRS="\
     ${ANDROID_BUILD_TOP}/external/icu/icu4j/main/classes/regiondata/src \
     ${ANDROID_BUILD_TOP}/external/icu/icu4j/main/classes/translit/src \
     "
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8100 -cp ${CLASSPATH} com.android.icu4j.srcgen.Icu4jTransform ${INPUT_DIRS} ${DEST_SRC_DIR}
-
-# Copy / transform the resources needed by the code.
-for INPUT_DIR in ${INPUT_DIRS}; do
-  RESOURCES=$(find ${INPUT_DIR} -type f | egrep -v '.*(\.java|\/package\.html|\/ICUConfig.properties)$')
-  for RESOURCE in ${RESOURCES}; do
-    SOURCE_DIR=$(dirname ${RESOURCE})
-    RELATIVE_SOURCE_DIR=$(echo ${SOURCE_DIR} | sed "s,${INPUT_DIR}/,,")
-    RELATIVE_DEST_DIR=$(echo ${RELATIVE_SOURCE_DIR} | sed "s,com/ibm/icu,android/icu,")
-    DEST_DIR=${DEST_RESOURCE_DIR}/${RELATIVE_DEST_DIR}
-    mkdir -p ${DEST_DIR}
-    cp $RESOURCE ${DEST_DIR}
-  done
-done
-
-# Create the ICUConfig.properties for Android.
-mkdir -p ${ANDROID_ICU4J_DIR}/resources/android/icu
-sed 's,com.ibm.icu,android.icu,' ${ANDROID_BUILD_TOP}/external/icu/icu4j/main/classes/core/src/com/ibm/icu/ICUConfig.properties > ${ANDROID_ICU4J_DIR}/resources/android/icu/ICUConfig.properties
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8100 -cp ${CLASSPATH} com.android.icu4j.srcgen.CaptureDeprecatedElements ${INPUT_DIRS}
