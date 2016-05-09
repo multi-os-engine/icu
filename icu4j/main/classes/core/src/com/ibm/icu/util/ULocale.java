e60c6371595e2613a0b3d46b1bd5380b18aa33c3
Neil Fuller <nfuller@google.com>
2016-05-09 13:55:42 +0100

    Switch ULocale from SimpleCache to SoftCache
    Bug: 28326526
    Change-Id: I5ee7d3b68d80947615341f90b75b965fa8fcfbe7

--- icu4j/main/classes/core/src/com/ibm/icu/util/ULocale.java
+++ icu4j/main/classes/core/src/com/ibm/icu/util/ULocale.java
@@ -24,13 +24,12 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
-import com.ibm.icu.impl.ICUCache;
 import com.ibm.icu.impl.ICUResourceBundle;
 import com.ibm.icu.impl.ICUResourceTableAccess;
 import com.ibm.icu.impl.LocaleIDParser;
 import com.ibm.icu.impl.LocaleIDs;
 import com.ibm.icu.impl.LocaleUtility;
-import com.ibm.icu.impl.SimpleCache;
+import com.ibm.icu.impl.SoftCache;
 import com.ibm.icu.impl.locale.AsciiUtil;
 import com.ibm.icu.impl.locale.BaseLocale;
 import com.ibm.icu.impl.locale.Extension;
@@ -100,7 +99,12 @@ public final class ULocale implements Serializable, Comparable<ULocale> {
     // using serialver from jdk1.4.2_05
     private static final long serialVersionUID = 3715177670352309217L;
 
-    private static ICUCache<String, String> nameCache = new SimpleCache<String, String>();
+    private static SoftCache<String, String, Void> nameCache = new SoftCache<String, String, Void>() {
+        @Override
+        protected String createInstance(String tmpLocaleID, Void unused) {
+            return new LocaleIDParser(tmpLocaleID).getName();
+        }
+    };
 
     /**
      * Useful constant for language.
@@ -290,7 +294,12 @@ public final class ULocale implements Serializable, Comparable<ULocale> {
         FORMAT
     }
 
-    private static final SimpleCache<Locale, ULocale> CACHE = new SimpleCache<Locale, ULocale>();
+    private static final SoftCache<Locale, ULocale, Void> CACHE = new SoftCache<Locale, ULocale, Void>() {
+        @Override
+        protected ULocale createInstance(Locale key, Void unused) {
+            return JDKLocaleHelper.toULocale(key);
+        }
+    };
 
     /**
      * Cache the locale.
@@ -427,12 +436,7 @@ public final class ULocale implements Serializable, Comparable<ULocale> {
         if (loc == null) {
             return null;
         }
-        ULocale result = CACHE.get(loc);
-        if (result == null) {
-            result = JDKLocaleHelper.toULocale(loc);
-            CACHE.put(loc, result);
-        }
-        return result;
+        return CACHE.getInstance(loc, null /* unused */);
     }
 
     /**
@@ -1112,12 +1116,7 @@ public final class ULocale implements Serializable, Comparable<ULocale> {
         } else {
             tmpLocaleID = localeID;
         }
-        String name = nameCache.get(tmpLocaleID);
-        if (name == null) {
-            name = new LocaleIDParser(tmpLocaleID).getName();
-            nameCache.put(tmpLocaleID, name);
-        }
-        return name;
+        return nameCache.getInstance(tmpLocaleID, null /* unused */);
     }
 
     /**
